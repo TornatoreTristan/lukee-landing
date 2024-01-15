474780aca3e3a927542432f1321b298eaf61d911
AgenceRevontuli <tristan@revontuli.fr>
2024-01-15 11:10:55 +0100

    Update content of the landing page

---
+++ src/components/Nav/DropDownMenu.vue
@@ -0,0 +1,97 @@
+<template>
+  <nav>
+    <HambugerMenuIcon @toggle="toggleMenu" class="nav-icon" />
+    <div class="main-nav" v-show="menuVisible">
+      <div class="content-nav">
+        <!-- <ul>
+          <li><a href="#">Catalogue</a></li>
+          <li><a href="#">Ressources</a></li>
+          <li><a href="#">FAQ</a></li>
+          <li><a href="#">À propos</a></li>
+        </ul> -->
+        <div class="cta-header">
+          <h2>Accédez à Lukee dès maintenant</h2>
+          <a href="https://app.lukee.io/signin" class="cta-ternary"
+            >Se connecter</a
+          >
+          <a href="https://app.lukee.io/signup" class="cta-primary"
+            >S'inscrire gratuitement</a
+          >
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import HambugerMenuIcon from "./HamburgerMenuIcon.vue";
+
+export default {
+  components: {
+    HambugerMenuIcon,
+  },
+  data() {
+    return {
+      menuVisible: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.menuVisible = !this.menuVisible;
+    },
+  },
+};
+</script>
+
+<style>
+.nav-icon {
+  z-index: 1;
+}
+.content-nav {
+  padding: 3rem;
+}
+.content-nav ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+.content-nav ul li {
+  margin-bottom: 2rem;
+}
+.content-nav ul li a {
+  font-size: 2rem;
+  text-decoration: none;
+}
+.main-nav {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  top: 0;
+  right: 0;
+  z-index: 1;
+  height: 100vh;
+  width: 100vw;
+  border: 4px solid #1a1f30;
+  border-radius: 25px;
+  color: #fff;
+  background-image: url("/banniere-lukee.svg");
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.cta-header {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  align-items: center;
+}
+.cta-ternary {
+  background-color: #fff;
+  color: #1a1f30;
+  padding: 1rem 2rem;
+  border-radius: 50px;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/Nav/HamburgerMenuIcon.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="menu-container" @click="toggleMenu">
+    <div class="bar1" :class="{ change: menuVisible }"></div>
+    <div class="bar2" :class="{ change: menuVisible }"></div>
+    <div class="bar3" :class="{ change: menuVisible }"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuVisible: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.menuVisible = !this.menuVisible;
+      this.$emit("toggle");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.menu-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 35px;
+  height: 26px;
+  cursor: pointer;
+  background-color: #fff;
+  padding: 0 1rem;
+  border-radius: 5px;
+  z-index: 2;
+}
+
+.menu-container .bar1,
+.menu-container .bar2,
+.menu-container .bar3 {
+  width: 35px;
+  height: 3px;
+  background-color: #333;
+  transition: 0.3s;
+  border-radius: 2px;
+}
+
+.menu-container .bar1.change {
+  transform: rotate(-45deg) translate(-6px, 6px);
+}
+
+.menu-container .bar2.change {
+  opacity: 0;
+}
+
+.menu-container .bar3.change {
+  transform: rotate(45deg) translate(-6px, -6px);
+}
+</style>
