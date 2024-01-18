b06f494ea329e0ead4fb45539f6cbc4725566698
AgenceRevontuli <tristan@revontuli.fr>
2024-01-18 08:43:16 +0100

    Add sitemap && robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@ import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import partytown from "@astrojs/partytown";
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://www.lukee.io/",
   integrations: [
     vue(),
     partytown({
@@ -11,5 +14,7 @@ export default defineConfig({
         forward: ["dataLayer.push"],
       },
     }),
+    robotsTxt(),
+    sitemap(),
   ],
 });
